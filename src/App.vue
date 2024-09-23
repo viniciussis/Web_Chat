abc13d4b06a146de4715bdccb5d925a901917e7c
viniciussis <vinicius01012@gmail.com>
2024-09-23 10:55:05 -0300

    /init: initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Web Chat</h1>
+  <div class="divider" />
+</template>
+
+<style lang="scss" scoped>
+.divider {
+  border-bottom: 1px solid white;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "src/assets/styles/mixins";
+          @use "src/assets/styles/variables";
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+})
